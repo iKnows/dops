cf9827da61c3d901c81221fcca97ffe77abf529a
ysicing <root@ysicing.net>
2017-12-26 22:58:12 +0800

    [add] update release.sh

--- dops/version.py
+++ dops/version.py
@@ -1,6 +1,6 @@
 VERSION_INFO = (
                 ("program","dops"),
                 ("version","dev-0.0.1"),
-                ("commit","0-c218079"),
-                ("buildTime","2017-12-26 22时49分08秒"),
+                ("commit","1.gab2529e-ab2529e"),
+                ("buildTime","2017-12-26 22时56分24秒"),
 )
