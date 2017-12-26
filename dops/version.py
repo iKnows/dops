9df42d3b4e51822c758d5a810725ff7550164087
ysicing <root@ysicing.net>
2017-12-26 23:24:52 +0800

    [add] test version

--- dops/version.py
+++ dops/version.py
@@ -1,7 +1,7 @@
 VERSION_INFO = (
                 ("program","dops"),
                 ("version","dev-0.0.1"),
-                ("pypi","0.0.1-3"),
-                ("commit","3.gfa5f1c1"),
-                ("buildTime","2017-12-26 23时02分32秒"),
+                ("pypi","0.0.1.5"),
+                ("commit","5.g396e7d4"),
+                ("buildTime","2017-12-26 23时24分30秒"),
 )
